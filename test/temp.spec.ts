10ec02861a6de0d335a9b5cbae0ec7a0877fa526
Brian Golden <brian.golden09@gmail.com>
2017-12-23 11:21:55 -0800

    Test with mocha and chai
      * run mocha tests as part of the build
      * add a temp test until more code exists
      * run against node 8 in CI
      * update typescript and tslint versions to latest

---
+++ test/temp.spec.ts
@@ -0,0 +1,12 @@
+/* tslint:disable:no-implicit-dependencies */
+import { expect } from "chai";
+import "mocha";
+/* tslint:enable:no-implicit-dependencies */
+
+describe("Hello function", () => {
+
+  it("should return hello world", () => {
+    expect("Hello world!").to.equal("Hello world!");
+  });
+
+});
