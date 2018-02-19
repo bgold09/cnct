96b7e9f2ab3072d3d09c88e94af63d4f2a34b646
Brian Golden <brian.golden09@gmail.com>
2018-02-19 14:54:54 -0800

    attempt at sh invoker

--- src/actions/ShellAction/ShellInvokerSH.ts
+++ src/actions/ShellAction/ShellInvokerSH.ts
@@ -1,7 +1,27 @@
+import { exec, ExecOptions, ExecOutputReturnValue } from "shelljs";
+import { promisify } from "util";
+import { ConsoleLogger } from "../../Logger/ConsoleLogger";
+import { ILogger } from "../../Logger/ILogger";
+import { IShellActionConfig } from "./IShellActionConfig";
 import { IShellInvoker } from "./IShellInvoker";
 
+// export const execAsync: (command: string, options: shelljs.ExecOptions)
+//  => Promise<shelljs.ExecOutputReturnValue> = promisify(shelljs.exec);
+const execAsync = promisify<string, ExecOptions, ExecOutputReturnValue>(exec);
+
 export class ShellInvokerSH implements IShellInvoker {
-    public async invokeShellAsync(): Promise<void> {
-        return Promise.resolve();
+
+    public constructor(
+        private readonly logger: ILogger = new ConsoleLogger()) {
+    }
+
+    public async invokeShellAsync(shellActionConfig: IShellActionConfig): Promise<void> {
+        const options: ExecOptions = {
+            silent: false,
+            async: false,
+        };
+
+        const r: ExecOutputReturnValue = await execAsync(shellActionConfig.command, options);
+        this.logger.logInfo(`code: ${r.code}; stdout: ${r.stdout}`);
     }
 }
